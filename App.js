3352300fed9e9da3a32cff29d95633f76e35e0f9
victor <vpinedaa@unah.hn>
2022-02-15 22:22:26 -0600

    set, get y remove ASYNC STORAGE

--- App.js
+++ App.js
@@ -1,112 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableHighlight,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [ inputTexto, setInputTexto ] = useState('')
+  const [ nombreStorage, setNombreStorage] = useState('')
+  
+  useEffect(() => {
+    obtenerDatos()
+  },[])
+  
+  const guardarDatos = async () =>{
+    try {
+      await AsyncStorage.setItem('nombre', inputTexto)
+      setNombreStorage(inputTexto)
+    } catch (error) {
+      console.log(error)      
+    }
+  }
+  const obtenerDatos = async () =>{
+    try {
+      const nombre = await AsyncStorage.getItem('nombre')
+      setNombreStorage(nombre)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const eliminarDatos = async () => {
+    try {
+      await AsyncStorage.removeItem('nombre')
+      setNombreStorage('')
+    } catch (error) {
+        console.log(error)
+    }
+  }
+  
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+    <>
+      <View style={styles.contenedor}>
+        { nombreStorage ? <Text>Hola: {nombreStorage}</Text> : null}
+        
+        <TextInput 
+          style={styles.input} 
+          placeholder="Escribe tu nombre"
+          onChangeText={ texto => setInputTexto(texto)}
+        />
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+        <Button 
+          title='Guardar'
+          color='#333'
+          onPress={ () => guardarDatos()}
+        />
+        { nombreStorage ? (
+          <TouchableHighlight 
+            style={styles.btnEliminar}
+            onPress={() => eliminarDatos()}
+          >
+            <Text style={styles.txtEliminar}>Eliminar Nombre &times;</Text>
+          </TouchableHighlight>
+        ): null}
+        
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  contenedor:{
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  input:{
+    borderColor: '#666',
+    borderBottomWidth: 1,
+    width: 300,
+    height: 40,
+    marginBottom: 5
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  btnEliminar:{
+    backgroundColor: 'red',
+    marginTop: 20,
+    padding: 10,
   },
-  highlight: {
-    fontWeight: '700',
-  },
+  txtEliminar:{
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  }
 });
 
 export default App;
